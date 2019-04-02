f059a2001d7c1598f068c6fd17fc3ee1f9d6dd8c
pete-lepusden <46822646+pete-lepusden@users.noreply.github.com>
2019-04-02 08:34:47 -0400

    Add files via upload

--- pdogen/pdogendb.php
+++ pdogen/pdogendb.php
@@ -141,7 +141,6 @@ echo $html;
 	  <meta name="description" content="">
 		<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	  <meta name="author" content="Michelle Woodruff">
 		<meta name="description" content="https://mwoodruff.net/articles/automation/time-saving-php-pdo-code-generator/">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	  </head>
